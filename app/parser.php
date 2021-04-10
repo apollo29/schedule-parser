3aefd43ede730b79834a48e60edef3964b995fe1
tdascoli <R0hrch@ch>
2021-04-10 15:52:45 +0200

    example

--- app/parser.php
+++ app/parser.php
@@ -4,7 +4,7 @@ use ScheduleParser\ScheduleParser;
 
 require '../vendor/autoload.php';
 
-$example = array(
+$schedules = array(
     "Vereinsnummer" => "10311",
     "VereinsId" => "1343",
     "notification" => "thomas.dascoli@gmail.com",
@@ -21,20 +21,6 @@ $example = array(
         )
     )
 );
-
-$schedules = array(
-    "Vereinsnummer" => "10330",
-    "VereinsId" => "1362",
-    "notification" => "thomas.dascoli@gmail.com",
-    "schedules" => array(
-        "default" => array(
-            "url" => "http://www.football.ch/portaldata/1/nisrd/WebService/verein/calendar.asmx/Verein?v=1362&away=1&sp=de&format=csv",
-            "table" => "spielplan",
-            "custom" => false
-        )
-    )
-);
-
 
 $parser = new ScheduleParser(__DIR__);
 
